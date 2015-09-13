2194105b1038c3bf1a4bbffadcf864264d1ca518
qianyu668899 <jinggeqianyu1991@gmail.com>
2015-09-13 15:51:42 -0400

    Simplify code using one controller and one service

--- angulartictactoe/static/js/app.js
+++ angulartictactoe/static/js/app.js
@@ -9,48 +9,84 @@ var app = angular.module('angulartictactoe', [
 /* Controllers*/
 var gameControllers = angular.module('gameControllers', []);
 
-gameControllers.controller('gameController',['$scope', '$http', function($scope, $http) {
-    $scope.intro = true;
-	$scope.introduction = "Tell you how to play the game";
-    $scope.winner = false;
-    $scope.winners = [];
-    
-    $scope.init_board = function () {
-        init_game($scope, $http);
-    }
+gameControllers.controller('gameController',['$scope', '$http', 'gameApiService', 
+    function($scope, $http, gameApiService) {
+      $scope.intro = true;
+	  $scope.introduction = "Tell you how to play the game";
+      $scope.winner = false;
+      $scope.winners = [];
+      $scope.history = [];
 
-	$scope.showIntro = function() {
+      gameApiService.history().success(function(data){
+        console.log(data);
+        $scope.history = data._history;
+      });
+    
+      $scope.init_board = function() {
+        $scope.winner = false;
+        $scope.cat = false;  
+        gameApiService.init().success(function(data) {
+            $scope.currentPlayer = data._current_player;
+            $scope.board = chunk(data._game_board, 3);
+            console.log($scope.board);
+        }); 
+    
+      }
+
+	  $scope.show_introduction = function() {
 	    $scope.intro = $scope.intro === false ? true : false;
 	    console.log("Show introduction");
-	}
+	  }
 
-	$scope.isTaken = function(cell) {
+	  $scope.is_taken = function(cell) {
 		$scope.flag = cell !== '_';
 		console.log($scope.flag);
         return $scope.flag;
-    };
+      };
 
-    $scope.update = function(rindex, cindex) {
+      $scope.update = function(rindex, cindex) {
         $scope.board[rindex][cindex] = $scope.currentPlayer;
         $scope.currentPlayer = $scope.currentPlayer === 'X' ? 'O' : 'X';
         console.log("cell:"+$scope.currentPlayer);
-        updateBoard(rindex*3 + cindex, $scope, $http);
-    };
-}]);
+        var index = rindex*3 + cindex;
+        gameApiService.update(index).success( function(data) {
+          if (data._winner) {
+            console.log("We have a winner.");
+            $scope.winner = true;
+            $scope.winners.push(data._current_player);
+            $scope.init_board();
+            return;
+          }
+        });
+      };
 
-gameControllers.controller('gameHistoryCtrl', ['$scope', '$http', function($scope, $http){
-    console.log("game history");
-    $http.get('http://0.0.0.0:6543/api/history').
-         success( function(data){
-            console.log(data);
-            $scope.history = data._history;
-    })
-
-    $scope.to_board = function(arr, size) {
+      $scope.to_board = function(arr, size) {
         return chunk(arr, size);
+      };
     }
-}]);
+]);
 
+/* App services*/
+var gameServices = angular.module('gameServices', []);
+
+gameServices.factory('gameApiService', ['$http', 
+    function($http){
+      var urlBase = "http://0.0.0.0:6543/api";
+      var gameApiService = {};
+      gameApiService.init = function() {
+        return $http.get(urlBase + '/init');
+      };
+      gameApiService.update = function(index) {
+        return $http.post(urlBase + '/update?index='+index);
+      };
+      gameApiService.history = function() {
+        return $http.get(urlBase + '/history');
+      };
+      return gameApiService;
+   }
+]);
+
+/* Utility functions */
 function chunk(arr, size) {
   var newArr = [];
   for (var i=0; i<arr.length; i+=size) {
@@ -58,45 +94,3 @@ function chunk(arr, size) {
   }
   return newArr;
 }
-
-function updateBoard(index, scope, http) {
-	http.get('http://0.0.0.0:6543/api/update?index='+index).
-        success(function(data) {
-          if (data._winner) {
-            console.log("We have a winner.");
-            scope.winner = true;
-            scope.winners.push(data._current_player);
-            init_game(scope, http);
-            //history();
-            return;
-          }
-        });
-}
-
-function init_game(scope, http){
-    scope.winner = false;
-    scope.cat = false;
-	http.get('http://0.0.0.0:6543/api/init').
-        success(function(data) {
-        	scope.currentPlayer = data._current_player;
-            scope.board = chunk(data._game_board, 3);
-            console.log(scope.board);
-    });
-}
-
-/* App services*/
-var gameServices = angular.module('gameServices', []);
-
-gameServices.factory('gameHistoryService',['$scope', '$http',function($http){
-    return {
-      history:function(){
-        $http.get('http://0.0.0.0:6543/api/history').
-         success( function(data){
-            console.log(data);
-            $scope.history = data;
-            return data;
-         })
-      }
-    };
- }
-]);
