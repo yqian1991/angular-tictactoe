03241236472d5f1b59dd2c3b0d72e31ec16ea2a3
qianyu668899 <jinggeqianyu1991@gmail.com>
2015-09-12 21:07:25 -0400

    first move around with angular ng-click

--- angulartictactoe/static/js/app.js
+++ angulartictactoe/static/js/app.js
@@ -1,1 +1,10 @@
 var app = angular.module('angulartictactoe', []);
+
+var gameController = function($scope) {
+    $scope.intro = true;
+    $scope.introButton = "How to play";
+	$scope.introduction = "Tell you how to play the game";
+	$scope.showIntro = function() {
+	    $scope.intro = $scope.intro === false ? true : false;
+	}
+}
